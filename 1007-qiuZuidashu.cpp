a5b4edb8a0ee9410403f39c605d61be602be4f0b
S3ner-CloudStudio <2671960@qq.com>
2020-10-07 11:30:52 +0800

    update

---
+++ 1007-qiuZuidashu.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+//求两个或者三个数中的最大值，用带有默认参数的函数实现
+
+int main (){
+
+    int max(int a,int b,int c=0);
+    int a,b,c;
+    cin >> a >> b >> c ;
+    cout << "max(a,b,c)=" << max(a,b,c) <<endl;
+    cout << "max(a,b)=" << max (a,b) << endl;
+    // cout << "max(a,c)" << max(a,c) <<endl;
+    
+
+    return 0;
+    /*
+    double a,b,c;
+    cout << "请输入两个或三个数"
+    cin >> a >> b >> c;
+    */
+}
+
+int max(int a,int b,int c  = 0){
+    if (b>a) a=b;
+    if (c>a) a=c;
+    return a;
+}
